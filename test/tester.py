d80b366fd4272acfed956275c302d96526c2a2ae
Amir Shamsi <emirshamsi.uni@gmail.com>
2023-01-25 09:54:24 +0330

    adding a function to calculate average accuracy of all 3 methods

--- test/tester.py
+++ test/tester.py
@@ -22,3 +22,25 @@ class Test:
             self.accuracy_arr.append(
                 100 if y_train_val == 0 else 100 - (abs(y_test_val - y_train_val) / y_train_val * 100) % 100)
         return self.accuracy_arr
+
+    def get_avg_accuracy(self):
+        return sum(self.accuracy_arr) / len(self.accuracy_arr)
+
+
+# for test first we load the results
+randF = open('../generators/generated-data/Random_Entries.txt').read().split('\n')
+firstPF = open('exec-data/FirstPart.txt').read().split('\n')
+secondPF = open('exec-data/SecondPart.txt').read().split('\n')
+thirdPF = open('exec-data/ThirdPart.txt').read().split('\n')
+
+# let's test
+tester = Test(function)
+
+tester.test(randF, firstPF, 19)
+print(f'average accuracy of 1st Part: {tester.get_avg_accuracy()}')
+
+tester.test(randF, secondPF, 19)
+print(f'average accuracy of 2th Part: {tester.get_avg_accuracy()}')
+
+tester.test(randF, thirdPF, 19)
+print(f'average accuracy of 3th Part: {tester.get_avg_accuracy()}')
